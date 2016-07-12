7054cc73633775a6d595dd41885d4fc6229a9450
Christina Hsieh <hsiehkunlun@hotmail.com>
2016-07-12 07:35:55 +0000

    timestamp microservcie

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require("express");
 var app = express();
+var path = require("path");
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+app.get('/:string', function (req, res) {
+  var str = req.params.string.replace(/%20/g, " ");
+  var unix = parseInt(str, 10) || Date.parse(str)/1000;
+  var natural = new Date(unix*1000).toDateString() || str;
+  var output = { unix: unix, natural: natural };
+  res.json(output);
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 8080!');
 });
